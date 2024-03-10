51489d3824d9cf46fa6d6ea0cae0220e1385d5fc
AKHTAR HUSSAIN <hussain2003akhtar@gmail.com>
2024-03-10 12:46:46 +0530

    added 3 info section below slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Speakers from './components/Speakerslide/speakerslide';
 import Sponsers from './components/Sponsorslide/sponsorslide';
 import HomeRegImg from './components/homeRegImg'
+import HomeInfoContent from './components/homeInfoContent';
+
 
 function App() {
   return (
-    <div className="">
+    <div className="bg-[#DEDFE2]">
        <HomeRegImg/>
        <Sponsers/>
        <Speakers/>
+       <HomeInfoContent/>
     </div>
   );
 }
---
+++ my-app/src/components/homeInfoContent.js
@@ -0,0 +1,53 @@
+import ismLogo from '../images/ism_logo.png'
+
+export default function HomeInfoContent() {
+    return (
+        <div className=" w-[100vw] px-[200px] pb-[3rem]">
+
+            <div className="w-[100%] bg-[#FFF] rounded-xl p-[2rem] shadow-md mt-[6rem]">
+                <h1 className="text-[2rem] font-bold text-center mb-[2rem]">About IIT (ISM)  Dhanbad </h1>
+
+                <img className="float-right" src={ismLogo}/>
+
+                <p className="text-justify">
+
+                    IIT ISM Dhanbad stands as a beacon of excellence in engineering education and research, tracing its roots back to 1926 as the Indian School of Mines. Integrated into the esteemed IIT system in 2016, it has since upheld its legacy of producing skilled professionals in mining, earth sciences, petroleum engineering, and related fields. With a comprehensive range of undergraduate, postgraduate, and doctoral programs, the institute emphasizes both theoretical knowledge and practical skills, complemented by state-of-the-art facilities and laboratories. Research at IIT ISM Dhanbad is at the forefront of addressing societal challenges, with a focus on sustainable mining practices, environmental engineering, and renewable energy.
+
+                    Moreover, IIT ISM Dhanbad actively fosters collaborations with industries, facilitating internships, placements, and research partnerships that bridge academia and industry. Its entrepreneurial ecosystem nurtures innovation, supporting start-ups and providing avenues for technology transfer and commercialization. Through global engagement initiatives and a commitment to social responsibility, the institute remains dedicated to producing graduates who are not only technically proficient but also socially conscious and globally aware, making significant contributions to both industry and society.
+                </p>
+
+            </div>
+
+
+            <div className="w-[100%] bg-[#FFF] rounded-xl p-[2rem] shadow-md mt-[6rem]">
+                <h1 className="text-[2rem] font-bold text-center mb-[2rem]">About IIT (ISM)  Dhanbad </h1>
+
+                <img className="float-right" src={ismLogo}/>
+
+                <p className="text-justify">
+
+                    IIT ISM Dhanbad stands as a beacon of excellence in engineering education and research, tracing its roots back to 1926 as the Indian School of Mines. Integrated into the esteemed IIT system in 2016, it has since upheld its legacy of producing skilled professionals in mining, earth sciences, petroleum engineering, and related fields. With a comprehensive range of undergraduate, postgraduate, and doctoral programs, the institute emphasizes both theoretical knowledge and practical skills, complemented by state-of-the-art facilities and laboratories. Research at IIT ISM Dhanbad is at the forefront of addressing societal challenges, with a focus on sustainable mining practices, environmental engineering, and renewable energy.
+
+                    Moreover, IIT ISM Dhanbad actively fosters collaborations with industries, facilitating internships, placements, and research partnerships that bridge academia and industry. Its entrepreneurial ecosystem nurtures innovation, supporting start-ups and providing avenues for technology transfer and commercialization. Through global engagement initiatives and a commitment to social responsibility, the institute remains dedicated to producing graduates who are not only technically proficient but also socially conscious and globally aware, making significant contributions to both industry and society.
+                </p>
+
+            </div>
+
+
+            <div className="w-[100%] bg-[#FFF] rounded-xl p-[2rem] shadow-md mt-[6rem] mb-[3rem]">
+                <h1 className="text-[2rem] font-bold text-center mb-[2rem]">About IIT (ISM)  Dhanbad </h1>
+
+                <img className="float-right" src={ismLogo}/>
+
+                <p className="text-justify">
+
+                    IIT ISM Dhanbad stands as a beacon of excellence in engineering education and research, tracing its roots back to 1926 as the Indian School of Mines. Integrated into the esteemed IIT system in 2016, it has since upheld its legacy of producing skilled professionals in mining, earth sciences, petroleum engineering, and related fields. With a comprehensive range of undergraduate, postgraduate, and doctoral programs, the institute emphasizes both theoretical knowledge and practical skills, complemented by state-of-the-art facilities and laboratories. Research at IIT ISM Dhanbad is at the forefront of addressing societal challenges, with a focus on sustainable mining practices, environmental engineering, and renewable energy.
+
+                    Moreover, IIT ISM Dhanbad actively fosters collaborations with industries, facilitating internships, placements, and research partnerships that bridge academia and industry. Its entrepreneurial ecosystem nurtures innovation, supporting start-ups and providing avenues for technology transfer and commercialization. Through global engagement initiatives and a commitment to social responsibility, the institute remains dedicated to producing graduates who are not only technically proficient but also socially conscious and globally aware, making significant contributions to both industry and society.
+                </p>
+
+            </div>
+
+        </div>
+    );
+}
