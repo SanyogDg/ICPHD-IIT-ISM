7990db320bb01fe949f5c04c3ecb337e4956f1b5
SanyogDg <sanyogdongrebhw@gmail.com>
2024-03-14 02:39:59 +0530

    random nvc

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -15,8 +15,8 @@ const Navabar = () => {
   return (
     <nav className={ `'container' ${Colornav? 'coloractive' : ''}` }
     >
-        <img src={logo} alt="" className='logo' />
-        <ul>
+        <img src={logo} alt="" className='logo ' />
+        <ul className='heading'>
             <NavLink className={(e)=>{return e.isActive?"red": ""}} to="/"><li>Home</li></NavLink>
             <NavLink className={(e)=>{return e.isActive?"red": ""}} to="/registration"><li>Registration</li></NavLink>
             <NavLink className={(e)=>{return e.isActive?"red": ""}} to="/themes"><li>Themes</li></NavLink>
