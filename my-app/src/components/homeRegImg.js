cc769f3dd3ce2d34098f09919e1ede185229c81e
kayashth1 <yashprof2004@gmail.com>
2024-06-02 00:52:35 +0530

    Improved responsivness and changed theme page

--- my-app/src/components/homeRegImg.js
+++ my-app/src/components/homeRegImg.js
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom'
+import './homeregimg.css';
 export default function homeImg()
 {
     function openPDF(){
@@ -10,16 +11,16 @@ export default function homeImg()
         bg-no-repeat bg-cover bg-center ">
             <div className="bgImg w-[100vw] h-[100vh] flex flex-row justify-center items-center ">
 
-            <div className="w-[50vw] flex flex-col items-center opacity-100">
+            <div className='w-[50vw] flex flex-col items-center opacity-100 respCon'>
 
-                <div className="text-[2rem] font-bold text-white text-center opacity-100"> Fueling Your Journey with Quality Petroleum Solutions
+                <div className='text-[2rem] font-bold text-white text-center opacity-100 resph'> Fueling Your Journey with Quality Petroleum Solutions
                 </div>
 
-                <div className="text-[1.2rem] font-semibold text-brown-500 text-center text-white">  
+                <div className='text-[1.2rem] font-semibold text-brown-500 text-center text-white respP'>  
                 Register now to access exclusive offers, expert advice, and personalized services tailored to your specific requirements.
                 </div>
                 
-                    <div className='flex align-center justify-center p-4'>  
+                    <div className='flex align-center justify-center p-4 respB'>  
                      <button className="my-[1rem] w-[10rem] h-[3rem] text-[1.5rem] font-semibold text-white bg-blue-500 rounded-[2rem] hover:bg-blue-700  hover:scale-110" onClick={openPDF}>Brochure</button> 
                     <Link to={"/registration"}> <button className="my-[1rem] mx-[1rem] w-[10rem] h-[3rem] text-[1.5rem] font-semibold text-white bg-blue-500 rounded-[2rem] hover:bg-blue-700  hover:scale-110">Register</button> </Link>
                 </div>
