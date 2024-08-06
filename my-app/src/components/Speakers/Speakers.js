ef04e94fa67348a0328d59af011cab3c6154db43
SanyogDg <sanyogdongrebhw@gmail.com>
2024-08-06 14:46:53 +0530

    Corrections in speakers

--- my-app/src/components/Speakers/Speakers.js
+++ my-app/src/components/Speakers/Speakers.js
@@ -6,20 +6,19 @@ import Kaustav from './Speakerimg/kaustav.png';
 import mazumdar from './Speakerimg/mazumdar.png'
 import chitwan from './Speakerimg/chitwan.png'
 import amitabh from './Speakerimg/amitabh.png'
-const Planetary = [
-    {
-      image: `${pallavi}`,
-      name: 'Dr. Pallavi Jain Govil',
-      designation: 'IAS, Director General of Directorate General of Hydrocarbons (DGH)',
-    },
-    {
-      image: `${saloma}`,
-      name: 'Shri Saloma Yomdo',
-      designation: 'Director (Exploration & Development), Oil India',
-    }
-];
+
   
 const speakersList = [
+  {
+    image: `${pallavi}`,
+    name: 'Dr. Pallavi Jain Govil',
+    designation: 'IAS, Director General of Directorate General of Hydrocarbons (DGH)',
+  },
+  {
+    image: `${saloma}`,
+    name: 'Shri Saloma Yomdo',
+    designation: 'Director (Exploration & Development), Oil India',
+  },
   {
     image:`${Kaustav}` ,
     name: 'Dr. Kaustav Nag',
@@ -47,28 +46,11 @@ const speakersList = [
 function Speakers() {
   return (
     <>
-      <div className='flex flex-col items-center mt-28'>
+      <div className='flex flex-col items-center mt-[9rem] mb-8'>
         <div className='text-center mt-4 mb-8'>
-          <h1 className='text-red-600 text-4xl font-bold'>Plenary Speakers</h1>
+          <h1 className='text-red-600 text-4xl font-bold'>Our Speakers</h1>
         </div>
-        <div className='flex justify-center w-full px-4'>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-screen-lg'>
-            {Planetary.map((speaker, index) => (
-              <SpeakerCard
-                key={index}
-                image={speaker.image}
-                name={speaker.name}
-                designation={speaker.designation}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className='flex flex-col items-center mt-20 mb-8'>
-        <div className='text-center mt-4 mb-8'>
-          <h1 className='text-red-600 text-4xl font-bold'>Keynote Speakers</h1>
-        </div>
-        <div className='flex justify-center w-full px-4'>
+        <div className='flex justify-center w-full px-4 mt-[1rem]'>
           <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-screen-lg'>
             {speakersList.map((speaker, index) => (
               <SpeakerCard
--- my-app/src/components/Speakerslide/speakerslide.js
+++ my-app/src/components/Speakerslide/speakerslide.js
@@ -1,70 +1,83 @@
 import React, { useState } from 'react';
-import Marquee from 'react-fast-marquee';
-import './speakerslide.css'
-import pallavi from '../Speakers/Speakerimg/pallvijain.png'
-import saloma from "../Speakers/Speakerimg/saloma.png"
+import './speakerslide.css';
+import pallavi from '../Speakers/Speakerimg/pallvijain.png';
+import saloma from '../Speakers/Speakerimg/saloma.png';
 import Kaustav from '../Speakers/Speakerimg/kaustav.png';
-import mazumdar from '../Speakers/Speakerimg/mazumdar.png'
-import chitwan from '../Speakers/Speakerimg/chitwan.png'
-import amitabh from '../Speakers/Speakerimg/amitabh.png'
-
+import mazumdar from '../Speakers/Speakerimg/mazumdar.png';
+import chitwan from '../Speakers/Speakerimg/chitwan.png';
+import amitabh from '../Speakers/Speakerimg/amitabh.png';
 
 const speakers = [
-    {
-        img: `${pallavi}`,
-        name:"Prof. Rakesh Godawat",
-    },
-    {
-        img: `${saloma}`,
-        name: 'Prof. Suddhasatwa Basu'
-    },
-    {
-        img: `${Kaustav}`,
-        name: 'Prof. G D Yadav'
-    },
-    {
-        img: `${mazumdar}`,
-        name: 'Prof. Hemanta Kumar Sarma'
-    },
-    {
-        img: `${chitwan}`,
-        name: 'Prof. K. K. Pant'
-    },
-    {
-        img: `${amitabh}`,
-        name: 'Prof. Shalivahan'
-    },
-    
+  {
+    img: `${pallavi}`,
+    name: 'Dr. Pallavi Jain Govil',
+  },
+  {
+    img: `${saloma}`,
+    name: 'Shri Saloma Yomdo',
+  },
+  {
+    img: `${Kaustav}`,
+    name: 'Dr. Kaustav Nag',
+  },
+  {
+    img: `${mazumdar}`,
+    name: 'Shri Sanjay Kumar Mazumder',
+  },
+  {
+    img: `${chitwan}`,
+    name: 'Shri Chitwan Garg',
+  },
+  {
+    img: `${amitabh}`,
+    name: 'Amitabh Pandey',
+  },
 ];
 
-
-
 function Speakers() {
-    
-    const [Hovered, setHovered] = useState(false);
-    const onhoverEnter=() => {
-        setHovered(true);
-    }
-    const onhoverLeave = () => {
-        setHovered(false);
-    }
+  const [hovered, setHovered] = useState(null);
 
-    return (
-        <div className='bodyspk flex justify-center align-center'>
-            <div className='headingspk text-3xl text-blue-800 mb-10  flex align-center justify-center mt-10 ml-2 mr-2 pr-2'>
-                Executive <br /> Speakers
+  const onHoverEnter = (index) => {
+    setHovered(index);
+  };
+  const onHoverLeave = () => {
+    setHovered(null);
+  };
+
+  return (
+    <div className='bodyspk flex flex-col items-center'>
+      <div className='headingspk text-3xl md:text-4xl lg:text-5xl text-blue-800 mb-10 mt-10'>
+        Our <br /> Speakers
+      </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+        {speakers.map((s, index) => (
+          <div
+            key={index}
+            className='flex flex-col items-center'
+            onMouseEnter={() => onHoverEnter(index)}
+            onMouseLeave={onHoverLeave}
+          >
+            <img
+              src={s.img}
+              alt={s.name}
+              className={`transition-all duration-300 ${
+                hovered === index ? 'w-36 h-36' : 'w-32 h-32'
+              } object-cover rounded-full`}
+            />
+            <div className='mt-4 text-center'>
+              <p
+                className={`text-lg md:text-xl ${
+                  hovered === index ? 'font-bold' : 'font-normal'
+                }`}
+              >
+                {s.name}
+              </p>
             </div>
-            {/* <Marquee pauseOnHover speed={10} > */}
-                {speakers.map((s) => (
-                    <div key={s} className={Hovered? 'imghovered' : 'normalimg'}>
-                        <div><img src={s.img} alt='image' className={Hovered? 'imglarge':'imgsmall'} onMouseEnter={onhoverEnter}  onMouseLeave={onhoverLeave}></img></div>
-                        <div className={Hovered?'paraactive':'paranonactive'} onMouseEnter={onhoverEnter}  onMouseLeave={onhoverLeave}><p>{s.name}</p></div>
-                    </div>
-                ))}
-            {/* </Marquee> */}
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-// mt-5 flex align-center justify-center font-bold text-xlmb-12
 
 export default Speakers;
